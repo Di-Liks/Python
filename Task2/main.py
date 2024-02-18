2337ea189cb8be555fc8cb516d56690e7785b1c3
Dill <60818081+Di-Liks@users.noreply.github.com>
2024-02-18 22:22:38 +0300

    Update main.py

--- Task2/main.py
+++ Task2/main.py
@@ -5,25 +5,34 @@ def unique(number_list):
             number_set.add(number)
     return number_set
 
-def bio():
-    n = int(input("Введите число элементов в генеалогическом дереве: "))
-    tree = {}
-    for i in range(1, n):
-        child, parent = input(f"Введите имя потомка и родителя ({i}): ").split()
-    tree[parent] = child
-    queue = sorted(list(tree.keys()))
-    while queue:
-        person = queue.pop(0)
-        print(f"{person}: ", end="")
-        children = tree[person].split()
-        if children:
-            queue.extend(children)
+#def bio():
+#    n = int(input("Введите число элементов в генеалогическом дереве: "))
+#    tree = {}
+#    for i in range(1, n):
+#        child, parent = input(f"Введите имя потомка и родителя ({i}): ").split()
+#    tree[parent] = child
+#    queue = sorted(list(tree.keys()))
+#    while queue:
+#        person = queue.pop(0)
+#        height = 0
+#        if person in tree:
+#            children = tree[person].split()
+#            if children:
+#                queue.extend(children)
+#                height += 1
+#        print(f"{person}: {height}")
+
+def height(man):
+    if man not in p_tree:
+        return 0
+    else:
+        return 1 + height(p_tree[man])
 
     ##############
     # MAIN BLOCK #
     ##############
 
-print("Выберите номер задания от 1 до 19")
+print("Выберите номер задания от 1 до 2")
 nn = int(input("Номер: "))
 if nn == 1:
     numbers = int(input('Введите количество чисел: '))
@@ -34,4 +43,15 @@ if nn == 1:
     result = unique(number_list)
     print(f'В списке {numbers} чисел, из которых {len(result)} различных.')
 if nn == 2:
-    bio()
+    p_tree = {}
+    n = int(input("Введите число элементов в генеалогическом древе: "))
+    for i in range(n - 1):
+        child, parent = input("Введите пару потомок - родитель: ").split()
+        p_tree[child] = parent
+
+    heights = {}
+    for man in set(p_tree.keys()).union(set(p_tree.values())):
+        heights[man] = height(man)
+
+    for key, value in sorted(heights.items()):
+        print(key, value)
